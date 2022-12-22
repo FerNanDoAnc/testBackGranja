c4fd0d0ad8a5ec24402c02d41829a9e80b67bb13
Fernando Ancajima <fernando.ancajima99@gmail.com>
2022-12-21 20:36:37 -0500

    express.json() as express.RequestHandler);

--- src/socket/arduino.ts
+++ src/socket/arduino.ts
@@ -16,7 +16,8 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json() as express.RequestHandler);
 // Routes
 app.use('/', routes);
 
